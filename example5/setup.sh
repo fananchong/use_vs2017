5dd9b5674b8858b04c6e55855ecad126b5b5c187
fananchong <fananchong@126.com>
2018-07-20 18:51:15 +0800

    up

--- example5/setup.sh
+++ example5/setup.sh
@@ -4,8 +4,10 @@ docker build -t vs2017_gdb .
 
 docker rm -f vs2017_gdb
 
-docker tag vs2017_gdb:latest fananchong/vs2017_gdb:latest
+docker run -d --restart always -p 1022:22 --name vs2017_gdb vs2017_gdb
 
-docker push fananchong/vs2017_gdb:latest
+#docker tag vs2017_gdb:latest fananchong/vs2017_gdb:latest
 
-docker run -d --restart always -p 1022:22 --name vs2017_gdb fananchong/vs2017_gdb:latest
+#docker push fananchong/vs2017_gdb:latest
+
+#docker run -d --restart always -p 1022:22 --name vs2017_gdb fananchong/vs2017_gdb:latest
